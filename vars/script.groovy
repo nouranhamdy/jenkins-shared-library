c29e2314c76428625f3c0e13b8caa94e6a1cf5ad
nouranhamdy <nouranhamdy4555@gmail.com>
2022-08-07 03:07:38 +0200

    first groovy script

--- vars/script.groovy
+++ vars/script.groovy
@@ -1,10 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(java.lang.String username, java.lang.String password){
-    sh """
-        docker build  -t nodeapp:v1 .
-        docker login -u $username -p $password
-        docker image tag nodeapp:v1 nouranhamdy1998/nodeapp:v1 
-        docker image push nouranhamdy1998/nodeapp:v1 
-    """
+def call(){
+    withCredentials([
+        usernamePassword(credentialsId: 'docker-credentials' , usernameVariable: 'username', passwordVariable: 'password')]) {
+        sh """
+            docker build  -t nodeapp:v1 .
+            docker login -u $username -p $password
+            docker image tag nodeapp:v1 nouranhamdy1998/nodeapp:v1 
+            docker image push nouranhamdy1998/nodeapp:v1 
+        """
+    }
 }
